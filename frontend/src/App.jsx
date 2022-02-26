d6084fa9f3c616d0201b1db760f82a103a49882c
jvicrosario1106 <jvrosario1106@gmail.com>
2022-02-26 17:41:23 +0800

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {Button, Typography,Container, TextField} from "@mui/material"
+import axios from "axios"
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const getTheaters = () => {
+
+    axios.get("http://localhost:8080/api/theaters").then((res)=>console.log(res.statusText)).catch(err=>console.log(err))
+
+  }
+
+  useEffect(() => {
+    
+    getTheaters()
+
+  }, []);
 
   return (
     <div>
